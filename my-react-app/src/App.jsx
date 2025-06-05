01813a8c41c2129a57c585f634fcad4a9591ec70
Renan <renan_moreira@usp.br>
2025-06-05 15:13:26 -0300

    mensagem

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -17,8 +17,19 @@ function App() {
           <h1 style={{color:"#5EDFFF"}}>Cursos</h1>
           <h1 style={{color:"#E41376"}}>Mais Populares</h1>
         </div>
-        <div className='cursos  '>
+        <div className='cursos'>
             <ImplementaCarrosel visivel={2} nItens={[0, 1, 2, 3, 4]} item={[<CardCurso/>, <CardCurso/>, <CardCurso/>, <CardCurso/>, <CardCurso/>]} />
+        </div>
+      </div>
+      <div className='conteiner'>
+        <div className='menssagem'>
+            <h3>É hora de investir no que realmente importa: você</h3>
+            <p>
+              Conhecimento que abre portas, amplia horizontes e transforma ideias em realidade.
+              Aqui, aprender programação não é um fardo: é uma jornada leve, prática e feita sob medida para você, com conteúdos claros, vídeos explicativos, quizzes e desafios interativos.
+              Seja no seu ritmo ou no seu tempo, o importante é começar. 
+            </p>
+            <a className='btn'>Começe agora</a>
         </div>
       </div>
     </div>
