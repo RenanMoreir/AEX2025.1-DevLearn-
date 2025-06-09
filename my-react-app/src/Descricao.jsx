70c4798f927bac63baa52237d21d084002de42fa
Renan <renan_moreira@usp.br>
2025-06-09 15:55:03 -0300

    Adicionado Conteudo e card na pagina do curso

---
+++ my-react-app/src/Descricao.jsx
@@ -0,0 +1,17 @@
+import { StrictMode } from 'react'
+import './style/descricao.css'
+
+function Descricao(){
+    return(
+    <div className='descricao'>
+        <h1 className='titulo'>🏆 Curso de Introdução à Programação com JavaScript</h1>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis consectetur adipiscing elit.
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis consectetur adipiscing elit.
+        </p>
+    </div>
+    );
+}
+export default Descricao;
--- my-react-app/src/curso.jsx
+++ my-react-app/src/curso.jsx
@@ -1,3 +1,21 @@
-import { useState } from 'react'
-import 'style/cursos.css'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './style/curso.css'
+import Footer from './Footer.jsx'
+import NavBar from './NavBar.jsx'
+import Descricao from './Descricao.jsx'
+import Resumo from './Resumo.jsx'
 
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <NavBar />
+    <div className='conteudo'>
+        <Descricao className='Descricao' />
+        <Resumo className='Resumo' />
+        {/*<BarraProgresso />
+        <Arcodeon />*/}
+    </div>
+    <Footer />
+  </StrictMode>,
+)
